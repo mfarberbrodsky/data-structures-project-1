89ec73e3e36177a03fc283e99202c0be9a7403d4
Maya Farber Brodsky <maya@farberbrodsky.com>
2020-04-06 12:44:09 +0300

    Add updated files from moodle

---
+++ CircularList.java
@@ -0,0 +1,46 @@
+/**
+ * Circular list
+ *
+ * An implementation of a circular list with  key and info
+ */
+
+public class CircularList {
+
+    public CircularList(int maxLen) {
+        // to be replaced by student code
+    }
+
+    /**
+     * public Item retrieve(int i)
+     *
+     * returns the item in the ith position if it exists in the list.
+     * otherwise, returns null
+     */
+    public Item retrieve(int i) {
+        return null;  // to be replaced by student code
+    }
+
+    /**
+     * public int insert(int i, int k, String s)
+     *
+     * inserts an item to the ith position in list  with key k and  info s.
+     * returns -1 if i<0 or i>n  or n=maxLen otherwise return 0.
+     */
+    public int insert(int i, int k, String s) {
+        return 42;    // to be replaced by student code
+    }
+
+    /**
+     * public int delete(int i)
+     *
+     * deletes an item in the ith posittion from the list.
+     * returns -1 if i<0 or i>n-1 otherwise returns 0.
+     */
+    public int delete(int i) {
+        return 42;    // to be replaced by student code
+    }
+
+}
+ 
+ 
+ 
---
+++ Item.java
@@ -0,0 +1,18 @@
+public class Item {
+
+    private int key;
+    private String info;
+
+    public Item(int key, String info) {
+        this.key = key;
+        this.info = info;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+}
