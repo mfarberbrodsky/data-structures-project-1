ca311e3b6ecf3b5ace5844adb547024987e38390
Maya Farber Brodsky <maya@farberbrodsky.com>
2020-05-01 12:38:35 +0300

    Fix modulu in CircularList, finish timing tests in Main (except rotations)

--- CircularList.java
+++ CircularList.java
@@ -1,15 +1,15 @@
 /**
  * Circular list
- *
+ * <p>
  * An implementation of a circular list with  key and info
  */
 
 public class CircularList {
-	
-	private int maxLen;
-	private Item[] arr;
-	private int len = 0;
-	private int start = 0;
+
+    private int maxLen;
+    private Item[] arr;
+    private int len = 0;
+    private int start = 0;
 
     public CircularList(int maxLen) {
         this.maxLen = maxLen;
@@ -18,76 +18,77 @@ public class CircularList {
 
     /**
      * public Item retrieve(int i)
-     *
+     * <p>
      * returns the item in the ith position if it exists in the list.
      * otherwise, returns null
      */
-    public Item retrieve(int i) { 
-    	if ((0 <= i) && (i < len))
-    		return arr[(start + i) % maxLen];
-    	else
-    		return null;
+    public Item retrieve(int i) {
+        if ((0 <= i) && (i < len))
+            return arr[getPos(i)];
+        else
+            return null;
     }
 
     /**
      * public int insert(int i, int k, String s)
-     *
+     * <p>
      * inserts an item to the ith position in list  with key k and  info s.
      * returns -1 if i<0 or i>n  or n=maxLen otherwise return 0.
      */
     public int insert(int i, int k, String s) {
-    	if ((0 <= i) && (i <= len) && (i < maxLen)) {
-    		if ((len - i) <= i) {
-    			for (int j = len - 1; j >= i ; j--) {
-    				arr[(start + j + 1) % maxLen] = arr[(start + j) % maxLen];
-    			}
-    			
-    		}
-    		else {
-    			for (int j = 0; j <= i-1 ; j++) {
-    				arr[(start + j - 1) % maxLen] = arr[(start + j) % maxLen];
-    			}
-    			start = ((start + maxLen - 1) % maxLen) ;
-    		}
-    		arr[(start + i) % maxLen] = new Item(k, s);
-    		
-    		len ++;
-    		return 0;
-    	}
-    	
-    	else
-    		return -1;
+        if ((0 <= i) && (i <= len) && (i < maxLen)) {
+            if ((len - i) <= i) {
+                for (int j = len - 1; j >= i; j--) {
+                    arr[getPos(j + 1)] = arr[getPos(j)];
+                }
+
+            } else {
+                for (int j = 0; j <= i - 1; j++) {
+                    arr[getPos(j - 1)] = arr[getPos(j)];
+                }
+                start = ((start + maxLen - 1) % maxLen);
+            }
+            arr[getPos(i)] = new Item(k, s);
+
+            len++;
+            return 0;
+        } else
+            return -1;
     }
 
     /**
      * public int delete(int i)
-     *
+     * <p>
      * deletes an item in the ith posittion from the list.
      * returns -1 if i<0 or i>n-1 otherwise returns 0.
      */
     public int delete(int i) {
-    	if ((0 <= i) && (i <= len - 1)) {
-    		if ((len - i) <= i) {
-    			for (int j = i; j < len - 1 ; j++) {
-    				arr[(start + j) % maxLen] = arr[(start + j + 1) % maxLen];
-    			}
-    			
-    			arr[(start + len - 1) % maxLen] = null;
-    			
-    		}
-    		else {
-    			for (int j = i-1; j >= 0 ; j--) {
-    				arr[(start + j + 1) % maxLen] = arr[(start + j) % maxLen];
-    			}
-    			start = ((start + maxLen + 1) % maxLen);
-    		}
-    		
-    		len --;
-    		return 0;
-    	}
-    	else {
-    		return -1;
-    	}
+        if ((0 <= i) && (i <= len - 1)) {
+            if ((len - i) <= i) {
+                for (int j = i; j < len - 1; j++) {
+                    arr[getPos(j)] = arr[getPos(j + 1)];
+                }
+                arr[getPos(len - 1)] = null;
+            } else {
+                for (int j = i - 1; j >= 0; j--) {
+                    arr[getPos(j + 1)] = arr[getPos(j)];
+                }
+                start = ((start + maxLen + 1) % maxLen);
+            }
+            len--;
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    private int getPos(int i) {
+        int modulu = (start + i) % maxLen;
+        if (modulu >= 0) {
+            return modulu;
+        } else {
+            return modulu + maxLen;
+        }
     }
 
 }
--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Question 1 - Advantages of CircularList");
         System.out.println("---------------------------------------");
-        for (int i=1; i<=10; i++) {
-            CircularList cList = new CircularList(10000*i);
+        for (int i = 1; i <= 10; i++) {
+            CircularList cList = new CircularList(10000 * i);
             long cStart = System.currentTimeMillis();
-            for (int m=0; m<10000*i; m++) {
+            for (int m = 0; m < 10000 * i; m++) {
                 cList.insert(m, m, "hello");
             }
             long cEnd = System.currentTimeMillis();
@@ -13,35 +13,62 @@ public class Main {
 
             TreeList tList = new TreeList();
             long tStart = System.currentTimeMillis();
-            for (int m = 0; m < 10000*i; m++) {
+            for (int m = 0; m < 10000 * i; m++) {
                 tList.insert(m, m, "hello");
             }
             long tEnd = System.currentTimeMillis();
             long tTime = tEnd - tStart;
 
-            System.out.println("i=" + i + ", cTime=" + cTime + ", tTime=" + tTime + ".");
+            System.out.println("i=" + i + ", CircularList time=" + cTime + ", TreeList time=" + tTime + ".");
         }
+        System.out.println();
 
         System.out.println("Question 2 - Advantages of TreeList");
         System.out.println("-----------------------------------");
-        for (int i=1; i<=10; i++) {
-            CircularList cList = new CircularList(10000*i);
+        for (int i = 1; i <= 10; i++) {
+            CircularList cList = new CircularList(10000 * i);
             long cStart = System.currentTimeMillis();
-            for (int m=0; m<10000*i; m++) {
-                cList.insert(m/2, m, "hello");
+            for (int m = 0; m < 10000 * i; m++) {
+                cList.insert(m / 2, m, "hello");
             }
             long cEnd = System.currentTimeMillis();
             long cTime = cEnd - cStart;
 
             TreeList tList = new TreeList();
             long tStart = System.currentTimeMillis();
-            for (int m = 0; m < 10000*i; m++) {
-                tList.insert(m/2, m, "hello");
+            for (int m = 0; m < 10000 * i; m++) {
+                tList.insert(m / 2, m, "hello");
             }
             long tEnd = System.currentTimeMillis();
             long tTime = tEnd - tStart;
 
-            System.out.println("i=" + i + ", cTime=" + cTime + ", tTime=" + tTime + ".");
+            System.out.println("i=" + i + ", CircularList time=" + cTime + ", TreeList time=" + tTime + ".");
         }
+        System.out.println();
+
+        System.out.println("Question 3 - Random inserts");
+        System.out.println("-----------------------------------");
+        for (int i = 1; i <= 10; i++) {
+            CircularList cList = new CircularList(10000 * i);
+            long cStart = System.currentTimeMillis();
+            for (int m = 0; m < 10000 * i; m++) {
+                int index = (int) (Math.random() * (m + 1));
+                cList.insert(index, m, "hello");
+            }
+            long cEnd = System.currentTimeMillis();
+            long cTime = cEnd - cStart;
+
+            TreeList tList = new TreeList();
+            long tStart = System.currentTimeMillis();
+            for (int m = 0; m < 10000 * i; m++) {
+                int index = (int) (Math.random() * (m + 1));
+                tList.insert(index, m, "hello");
+            }
+            long tEnd = System.currentTimeMillis();
+            long tTime = tEnd - tStart;
+
+            System.out.println("i=" + i + ", CircularList time=" + cTime + ", TreeList time=" + tTime + ".");
+        }
+        System.out.println();
     }
 }
